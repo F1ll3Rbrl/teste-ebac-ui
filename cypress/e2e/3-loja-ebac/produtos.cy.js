953606f509cce50dff0737d145e0d905e4aa1e89
F1ll3Rbrl <olivei1@hotmail.com>
2025-12-13 03:08:39 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,48 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     it('Deve selecionar um produto da lista', () => {
         cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(3)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
-
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
 
     });
-    
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Tiberius Gym Tank'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página de produto', () => {
+        produtosPage.visitarProduto('Tiberius Gym Tank')
+        cy.get('.product_title').should('contain', 'Tiberius Gym Tank')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Erica Evercool Sports Bra')
+        produtosPage.addProdutoCarrinho('M', 'Orange', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Erica Evercool Sports Bra” foram adicionados no seu carrinho.')  
+    });
+
+        it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+
+        cy.get('.woocommerce-message').should('contain',  dados[0].nomeProduto)
+        })
+
+     });
 });
